524ab061104064545ba040f3ab9d6b5879babecd
Mavis Lai <laim3@bxscience.edu>
2025-11-11 10:51:05 -0500

    fixed up question one

--- java101/README.md/PracticeProblems.java
+++ java101/README.md/PracticeProblems.java
@@ -1,15 +1,16 @@
 public class PracticeProblems {
     public static void main(String[] args) {
         static int countOccurrences (int n) {
-            static int[] arr = new int[100];
-            int count = 0
-            for (int index = 0, index < 100, index += 1) {
+            int[] arr = new int[100];
+            int count = 0;
+            for (int index = 0; index < 100; index += 1) {
                 if (arr[index]/n == 1) {
-                    count += 1
+                    count += 1;
                 }
 
 
         }
    
-    }
+        
+    }  
 }

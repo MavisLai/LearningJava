1f47e379da61a762ee786abbd20bfc244b2ce9a2
Mavis Lai <laim3@bxscience.edu>
2025-11-11 10:35:47 -0500

    Finished question 1

---
+++ java101/README.md/PracticeProblems.java
@@ -0,0 +1,15 @@
+public class PracticeProblems {
+    public static void main(String[] args) {
+        static int countOccurrences (int n) {
+            static int[] arr = new int[100];
+            int count = 0
+            for (int index = 0, index < 100, index += 1) {
+                if (arr[index]/n == 1) {
+                    count += 1
+                }
+
+
+        }
+   
+    }
+}

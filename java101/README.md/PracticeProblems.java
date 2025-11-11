2a43f502920552ba6ed10a6bb247dc0e1162041c
Mavis Lai <laim3@bxscience.edu>
2025-11-11 16:35:18 -0500

    Fixing up problem one, got stuck on problem two

--- java101/README.md/PracticeProblems.java
+++ java101/README.md/PracticeProblems.java
@@ -1,16 +1,22 @@
 public class PracticeProblems {
-    public static void main(String[] args) {
+    public static void main(String[] args) {}
+    
         static int countOccurrences (int n) {
+
             int[] arr = new int[100];
             int count = 0;
+
             for (int index = 0; index < 100; index += 1) {
                 if (arr[index]/n == 1) {
                     count += 1;
                 }
+                else{
+                    count += 0;
+                }
+            }
 
-
+            return count;
         }
-   
         
     }  
 }
